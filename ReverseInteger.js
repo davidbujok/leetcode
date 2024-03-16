a0e8372ac52f25c0663c3e0d005da8e81f085699
davidbujok <davidbujok01@gmail.com>
2024-03-16 18:16:50 +0000

    Neat and performant solution using Math only.

--- ReverseInteger.js
+++ ReverseInteger.js
@@ -57,6 +57,21 @@ var reverse = function(x) {
   return absReversed * Math.sign(x);
 };
 
+// Neat and performant solution using Math only. 
+function reverse(x) {
+  const isNegative = x < 0;
+  x = Math.abs(x);
+  let ret = 0;
+  while (x > 0) {
+    const num = x % 10;
+    x = Math.floor(x / 10);
+    ret *= 10;
+    ret += num;
+  }
+  if (ret > Math.pow(2,31)) return 0;
+  return isNegative ? ret * -1 : ret;
+};
+
 console.log(reverseIntMath(129))
 console.log(reverseIntMath(-523))
 console.log(reverseIntMath(1534236469));

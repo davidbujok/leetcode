c52277768f2b1b2bc54bce3a7f1031b47fa4f65f
davidbujok <davidbujok01@gmail.com>
2024-03-23 13:46:37 +0000

    romanToInt backward solution

--- romanToInt.js
+++ romanToInt.js
@@ -63,9 +63,40 @@ var romanToIntEasyTwo = function(s) {
     return num;    
 };
 
-romanToInt("III")
-romanToInt("LVIII")
-romanToInt("MCMXCIV")
-romanToIntEasy("III")
-romanToIntEasy("LVIII")
-romanToIntEasy("MCMXCIV")
+const romanToIntBackward = s => {
+  if (!s || s.length === 0) {
+    return 0;
+  }
+
+  const map = new Map([['I', 1], ['V', 5], ['X', 10], ['L', 50], ['C', 100], ['D', 500], ['M', 1000]]);
+
+  let i = s.length - 1;
+  let result = map.get(s[i]);
+
+  while (i > 0) {
+    const curr = map.get(s[i]);
+    const prev = map.get(s[i - 1]);
+
+    if (prev >= curr) {
+      result += prev;
+    } else {
+      result -= prev;
+    }
+
+    i--;
+  }
+
+  console.log(result)
+  return result;
+};
+
+
+// romanToInt("III")
+// romanToInt("LVIII")
+// romanToInt("MCMXCIV")
+// romanToIntEasy("III")
+// romanToIntEasy("LVIII")
+// romanToIntEasy("MCMXCIV")
+romanToIntBackward("III")
+romanToIntBackward("LVIII")
+romanToIntBackward("MCMXCIV")
